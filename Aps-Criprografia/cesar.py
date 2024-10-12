4bd88b47ab20da791f2d6bcbb78a554e1fc78900
Nickolas Passos Homem de Mello <nickolasphmello10@gmail.com>
2024-10-12 15:48:51 -0300

    cesar-code
    Aqui está somente o codigo da criptografia usando a cifra de cesar.

---
+++ Aps-Criprografia/cesar.py
@@ -0,0 +1,35 @@
+def cripto_decripto(txt, key, funcao):
+  resultado = ""
+  for letra in txt:
+    if letra.isalpha():
+      limite = 65 if letra.isupper() else 96
+      letraNova = chr((ord(letra) - limite + (key * funcao)) % 26 + limite)
+      resultado += letraNova
+    else:
+      resultado += letra
+  return resultado
+
+def userface():
+  print(" \n Escolha uma das opcções : ")
+  print()
+  print("  Criptografar   == 1 ")
+  print(" Descriptografar == 2")
+  print()
+  funcao = input(" Qual a sua escolha ? ")
+
+  if funcao not in ['1', '2']:
+    print(" Opção Inválida, favor seguir as opções mostradas ")
+    return
+  
+  txt = input(" \n Informe a mensagem : ")
+  key = int(input(" Informe o número da codificação : "))
+
+  if funcao == '1':
+    print("\n Fras Criptografada : ", cripto_decripto(txt, key, 1))
+  elif funcao == '2':
+    print("\n Fras Criptografada : ", cripto_decripto(txt, key, -1))
+  else:
+    print("Opção inválida!")
+
+if __name__ == "__main__":
+  userface()
